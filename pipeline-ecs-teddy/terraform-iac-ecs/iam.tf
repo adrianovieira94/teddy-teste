287561bd5ef29956f16609846bf457c77cc42806
adriano <adriano.morais@caf.io>
2025-12-16 22:59:19 -0300

    fix: DEPLOY nginx

--- pipeline-ecs-teddy/terraform-iac-ecs/iam.tf
+++ pipeline-ecs-teddy/terraform-iac-ecs/iam.tf
@@ -1,28 +1,29 @@
-
 resource "aws_iam_role" "ecs_task_execution" {
-	name = "ecsTaskExecutionRole"
-	assume_role_policy = jsonencode({
-		Version = "2012-10-17"
-		Statement = [{
-			Effect = "Allow"
-			Principal = {
-				Service = "ecs-tasks.amazonaws.com"
-			}
-			Action = "sts:AssumeRole"
-		}]
-	})
-	tags = {
-		Environment = var.environment
-	}
+  name = "ecsTaskExecutionRole-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Environment = var.environment
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-	role       = aws_iam_role.ecs_task_execution.name
-	policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# Permissãopara acessar ECR
+# Permissão para acessar ECR
 resource "aws_iam_role_policy_attachment" "ecs_task_ecr_readonly" {
-	role       = aws_iam_role.ecs_task_execution.name
-	policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
