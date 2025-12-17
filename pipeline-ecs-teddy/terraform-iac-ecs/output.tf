3ef0077135d92dac603827ef4c5fa281348522ba
adriano <adriano.morais@caf.io>
2025-12-16 21:07:11 -0300

    fix: DEPLOY INFRA

--- pipeline-ecs-teddy/terraform-iac-ecs/output.tf
+++ pipeline-ecs-teddy/terraform-iac-ecs/output.tf
@@ -1,5 +1,14 @@
-
 output "ecs_cluster_name" {
-	description = "Nome do cluster ECS criado"
-	value       = var.cluster_name
+  description = "The name of the ECS cluster"
+  value       = module.ecs.cluster_name
+}
+
+output "ecs_service_name" {
+  description = "The name of the ECS service"
+  value       = module.ecs.services["app"].name
+}
+
+output "ecr_repository_url" {
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.app.repository_url # Assumindo que você tem um recurso aws_ecr_repository.app
 }
